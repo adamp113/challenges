bf5b46c1099567de25c4b06ab281a6b167f0c214
Adam Peterson <adamp113@gmail.com>
2020-09-19 09:47:00 -0400

    e1.9-10

---
+++ e1.10_roman_numerals.rb
@@ -0,0 +1,30 @@
+class Integer
+  ROMAN_NUMERALS = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+  }
+
+  def to_roman
+    result = ''
+    number = self
+    ROMAN_NUMERALS.each do |key, value|
+      multiplier, remainder = number.divmod(value)
+      if multiplier > 0
+        result += (key * multiplier)
+      end
+      number = remainder
+    end
+    result
+  end
+end
